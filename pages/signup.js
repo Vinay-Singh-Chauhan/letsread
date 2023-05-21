11e752cf870948954da7922c69898b748fcf27c3
Vinay Singh Chauhan <chauhansvinay@gmail.com>
2023-05-21 22:56:53 +0530

    sign up button

--- pages/signup.js
+++ pages/signup.js
@@ -68,7 +68,7 @@ const Signup = () => {
         <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
         <input onChange={onChangeInput} type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
       </div>
-      <button onClick={onSignUp} className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+      <button onClick={onSignUp} className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Sign Up</button>
       <p className="text-xs text-gray-500 mt-3">Already have an account, {<Link href={"/login"} className=' text-blue-600 underline'>Login</Link>}</p>
     </div>
   </div>
